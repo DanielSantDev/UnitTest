21d7f8ba0ecb64de2a5ca375faacb1e335fc97b4
Daniel Santana <daniel10gja@gmail.com>
2021-11-06 02:05:28 -0300

    Implementando Testes

---
+++ src/CalculadoraTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void SOMA() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void MULTIPLICA() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void SUBTRAI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void DIVIDE() {
+    }
+}
